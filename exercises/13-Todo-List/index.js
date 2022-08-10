9146186ad7f1aac94a77b0c22c46ad2e06be9c7f
JBritton2343 <jbritton854@gmail.com>
2022-08-10 15:18:12 +0000

    finished delete logic and started add logic

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,17 @@
 // Your code here
+//task deletion
+
+let rToDo = document.getElementById("addToDo");
+rToDo.addEventListener("click", "i", ()=>{
+    rToDo.removechild(rToDo.childNodes[i]);
+})
+
+//add task
+let addBtn = document.createElement("button")
+addBtn.innerHTML = "add";
+addBtn.name = "Add";
+document.body.appendChild(addBtn)
+let addToDo = document.getElementById("addToDo");
+addBtn.addEventListener("click", "li", ()=>{
+    
+})
