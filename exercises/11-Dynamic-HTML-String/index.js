0ffcc47e3802c9957381b98495b0410191b201fd
JBritton2343 <jbritton854@gmail.com>
2022-08-10 10:24:05 +0000

    made date.prototype.getFullYear part of the string

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,6 +1,6 @@
 let myString = "<p>Hello!</p> <strong>My friend</strong>";
 document.write(myString);
-let fullYear = "<p>We are in the year </p> " + Date.prototype.getFullYear()
+let fullYear = "<p>We are in the year </p> Date.prototype.getFullYear()"
 
 let myString2 = myString.concat(fullYear)
 document.write(myString2);
