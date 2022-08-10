a9bc37347dcd8690cb3118d6d5bcf8fcbbab31b2
JBritton2343 <jbritton854@gmail.com>
2022-08-10 10:28:14 +0000

    added year, mySecondString to existing code

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,6 +1,7 @@
 let myString = "<p>Hello!</p> <strong>My friend</strong>";
 document.write(myString);
-let fullYear = "<p>We are in the year </p> Date.prototype.getFullYear()"
-
-let myString2 = myString.concat(fullYear)
+let newString = "<p>We are in the year</p>"
+let year = Date.prototype.getFullYear();
+let mySecondString = newString.concat(year)
+let myString2 = myString.concat(mySecondString)
 document.write(myString2);
