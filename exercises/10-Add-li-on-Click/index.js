328c48948466d34b9625c119c0e64c05112d37a3
JBritton2343 <jbritton854@gmail.com>
2022-08-10 09:52:53 +0000

    added a setAttribute line

--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -2,9 +2,10 @@ let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
 	addElement = function(){
-	let ul = document.getElementById("#myList");
+	let ul = document.getElementById("myList");
 	let li = document.createElement("li");
 	let child = ul.children.length+1;
+	li.setAttribute("id" , "fourth element" + child);
 	li.appendChild(document.createTextNode("Fourth Element"+child));
 	ul.appendChild(li);
 	}
