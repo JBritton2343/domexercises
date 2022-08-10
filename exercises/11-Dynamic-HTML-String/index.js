ae44639b680e9310a59897f7060c5e4ca75f7e14
JBritton2343 <jbritton854@gmail.com>
2022-08-10 11:01:43 +0000

    removed first docment.write

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,5 +1,4 @@
 let myString = "<p>Hello!</p> <p><strong>My friend</strong> we are in the year </p>"
-document.write(myString);
 let date = new Date();
 let year = date.getFullYear();
 let string2 = myString.concat(year)
