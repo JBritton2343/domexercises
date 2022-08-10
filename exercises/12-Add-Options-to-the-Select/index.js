5cfe1dc2b2e818fa644269b5ab7899d16f19c937
JBritton2343 <jbritton854@gmail.com>
2022-08-10 11:56:32 +0000

    added value line

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -4,7 +4,8 @@ let mySelect = document.getElementById("mySelect");
 for(let i = 0; i < countries.length; i++){
     let select = document.createElement("option");
     select.textContent(countries[i]);
-    select.appendChild(mySelect);
+    select.value=countries[i];
+    mySelect.appendChild(select);
 };
 mySelect.addEventListener("change", fuction())
     alert(mySelect.selected);
