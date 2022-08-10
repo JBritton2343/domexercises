1cdd425cea995435f48d8cd1c92d5ff11ee78731
JBritton2343 <jbritton854@gmail.com>
2022-08-10 09:46:29 +0000

    added function blocking to the code

--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -1,8 +1,10 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+	addElement = function(){
 	let ul = document.getElementById("#myList");
 	let li = document.createElement("li");
 	li.appendChild(document.createTextNode("Fourth Element"));
 	ul.appendChild(li);
+	}
 });
