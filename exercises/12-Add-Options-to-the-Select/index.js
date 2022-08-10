dc7b2a66d17fa87618539da9105bf25d6175b17b
JBritton2343 <jbritton854@gmail.com>
2022-08-10 14:11:52 +0000

    updated line 5 changing arr to countries

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -2,7 +2,7 @@ let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Vene
 // your code here
 let mySelect = document.getElementById("mySelect");
 for(let i = 0; i < countries.length; i++){
-    if(mySelect.querySelectorAll('option[value="' + arr[i] + "\"]").length == 0) {
+    if(mySelect.querySelectorAll('option[value="' + countries[i] + "\"]").length == 0) {
         let selections = document.createElement("option");
         selections.textContent = countries[i];
         selections.value = countries[i];
