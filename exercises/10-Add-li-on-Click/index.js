2bdd03bc1057ebd998eabb36027cb64e97caea8f
JBritton2343 <jbritton854@gmail.com>
2022-08-10 09:54:43 +0000

    removed semi-colons

--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -4,8 +4,8 @@ button.addEventListener("click", function() {
 	addElement = function(){
 	let ul = document.getElementById("myList");
 	let li = document.createElement("li");
-	let child = ul.children.length+1;
-	li.setAttribute("id" , "fourth element" + child);
+	let child = ul.children.length+1
+	li.setAttribute("id" , "fourth element" + child)
 	li.appendChild(document.createTextNode("Fourth Element"+child));
 	ul.appendChild(li);
 	}
