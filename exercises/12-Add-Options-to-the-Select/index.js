c546007cd90dee657b9a9926b183c7a514b15616
JBritton2343 <jbritton854@gmail.com>
2022-08-10 11:48:43 +0000

    added quote marks to mySelect line 3

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,6 +1,6 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
-let mySelect = document.getElementById(mySelect);
+let mySelect = document.getElementById("mySelect");
 for(let i = 0; i < countries.length; i++){
     let select = document.createElement("option");
     select.textContent(countries[i]);
