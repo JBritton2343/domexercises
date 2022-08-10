f5522ba083980e6dc9c57d6f0f1689380368a45f
JBritton2343 <jbritton854@gmail.com>
2022-08-10 10:55:53 +0000

    deleted code for date

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,5 +1,2 @@
 let myString = "<p>Hello! <strong>My friend</strong> we are in the year </p>"
-const currentDate = date();
-const year = currentDate.getFullYear()
-year.concat(myString) 
 document.write(myString);
