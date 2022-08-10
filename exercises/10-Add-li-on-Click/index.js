6b57c2eddd8dd140d3669420ad3c6ad39a8df3be
JBritton2343 <jbritton854@gmail.com>
2022-08-10 10:04:17 +0000

    altered code

--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -1,12 +1,10 @@
 let button = document.querySelector("#superDuperButton");
+let ul = document.querySelector('myList')
 button.addEventListener("click", function() {
 	//your code here
 	addElement = function(){
-	let ul = document.getElementById("myList");
 	let li = document.createElement("li");
-	let child = ul.children.length+1
-	li.setAttribute("id" , "fourth element" + child)
-	li.appendChild(document.createTextNode("Fourth Element"+child));
+	li.appendChild(document.createTextNode("Fourth Element"))
 	ul.appendChild(li);
 	}
 });
