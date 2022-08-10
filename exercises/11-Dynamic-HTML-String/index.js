9eac723e2291d77e468bd12f069a580343289b6a
JBritton2343 <jbritton854@gmail.com>
2022-08-10 11:00:56 +0000

    readded date, year and string2 and wrote to doc

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,2 +1,6 @@
 let myString = "<p>Hello!</p> <p><strong>My friend</strong> we are in the year </p>"
 document.write(myString);
+let date = new Date();
+let year = date.getFullYear();
+let string2 = myString.concat(year)
+document.write(string2);
