850f3988c2abdf55db7b09a6a402b87a1cab7ad4
JBritton2343 <jbritton854@gmail.com>
2022-08-10 11:05:14 +0000

    reversed concat

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,5 +1,5 @@
 let myString = "<p>Hello!</p> <p><strong>My friend</strong> we are in the year </p>"
 let date = new Date();
 let year = date.getFullYear();
-let string2 = myString.concat(year)
+let string2 = year.concat(myString)
 document.write(string2);
