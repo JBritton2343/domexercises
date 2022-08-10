36dc6c14ad74a20381fa8289f87ba1bd6bd60213
JBritton2343 <jbritton854@gmail.com>
2022-08-10 10:38:24 +0000

    added semicolons

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,4 +1,4 @@
 let myString = "<p>Hello!</p> <strong>My friend</strong> <p>we are in the year </p>";
-let year = Date.prototype.getFullYear()
-let myNewString = myString.concat(year)
+let year = Date.prototype.getFullYear();
+let myNewString = myString.concat(year);
 document.write(myNewString);
