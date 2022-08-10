712def392a30ebccc2e35b8ef731d1a1fcb487e1
JBritton2343 <jbritton854@gmail.com>
2022-08-10 11:58:38 +0000

    added spaces line 7

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -4,7 +4,7 @@ let mySelect = document.getElementById("mySelect");
 for(let i = 0; i < countries.length; i++){
     let select = document.createElement("option");
     select.textContent(countries[i]);
-    select.value=countries[i];
+    select.value = countries[i];
     mySelect.appendChild(select);
 };
 mySelect.addEventListener("change", fuction())
