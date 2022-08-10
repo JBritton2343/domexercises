cbc63680c0c2e832091e9eeda48caf06e7b7dabd
JBritton2343 <jbritton854@gmail.com>
2022-08-10 10:20:56 +0000

    concated fullYear variable to myString variable

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,2 +1,4 @@
 let myString = "<p>Hello!</p> <strong>My friend</strong>";
 document.write(myString);
+let fullYear = "<p>We are in the year </p> " + Date.prototype.getFullYear()
+myString.concat(fullYear);
