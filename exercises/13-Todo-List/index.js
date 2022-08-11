d1e2a00ad8db00ce50702d1eac50bcc21b76b3c8
JBritton2343 <jbritton854@gmail.com>
2022-08-11 00:40:26 +0000

    added onKeyPress event and trash icon

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -9,16 +9,16 @@ rToDo.forEach(element => {
 });
 
 //add task
-let addBtn = document.createElement("button")
-addBtn.innerHTML = "add";
-addBtn.name = "Add";
-document.body.appendChild(addBtn)
+
 let addToDo = document.getElementById("addToDo");
-addBtn.addEventListener("click", "li", ()=>{
-    let newLi = document.createElement("li")
+let ul = document.querySelector("ul");
+addToDo.addEventListener("keypress", (enter)=>{
+    if(enter.key == "Enter"){
+        let newLi = document.createElement("li")
     newLi.innerHTML=addToDo.value;
     let newI = document.createElement("i");
     newI.classList.add("fa","fa-trash");
-    let ul = document.querySelector("ul");
+    newLi.appendChild(newI);
     ul.appendChild(newLi);
+    }
 })
