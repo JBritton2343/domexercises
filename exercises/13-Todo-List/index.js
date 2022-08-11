baedfb70c9b030382bf08bcc21801bd69d3c8fa5
JBritton2343 <jbritton854@gmail.com>
2022-08-11 00:30:32 +0000

    added add button functionality and deletions

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,10 +1,12 @@
 // Your code here
 //task deletion
 
-let rToDo = document.getElementById("addToDo");
-rToDo.addEventListener("click", "i", ()=>{
-    rToDo.removechild(rToDo.childNodes[i]);
-})
+let rToDo = document.querySelectorAll(".fa-trash");
+rToDo.forEach(element => {
+    element.addEventListener("click", (e)=>{
+        e.parentNode.removeChild(e)
+    })
+});
 
 //add task
 let addBtn = document.createElement("button")
@@ -13,5 +15,10 @@ addBtn.name = "Add";
 document.body.appendChild(addBtn)
 let addToDo = document.getElementById("addToDo");
 addBtn.addEventListener("click", "li", ()=>{
-    
+    let newLi = document.createElement("li")
+    newLi.innerHTML=addToDo.value;
+    let newI = document.createElement("i");
+    newI.classList.add("fa","fa-trash");
+    let ul = document.querySelector("ul");
+    ul.appendChild(newLi);
 })
