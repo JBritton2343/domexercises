0ca74576cc2d0f1b5cd0ad7b5e609ec9c3e10c5d
JBritton2343 <jbritton854@gmail.com>
2022-08-11 00:57:53 +0000

    added before end property to insertAdjacentHTML

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -16,12 +16,11 @@ addToDo.addEventListener("keypress", (enter)=>{
     if(enter.key == "Enter"){
         let newSpan = document.createElement("span");
         let newLi = document.createElement("li");
-    
-    let newI = document.createElement("i");
-    newI.classList.add("fa","fa-trash");
-    newSpan.appendChild(newI);
-    newLi.appendChild(newSpan);
-    ul.appendChild(newLi);
-    newLi.innerHTML=addToDo.value;
+        let newI = document.createElement("i");
+        newI.classList.add("fa","fa-trash");
+        newSpan.appendChild(newI);
+        newLi.appendChild(newSpan);
+         ul.appendChild(newLi);
+        newLi.insertAdjacentHTML("beforeend",addToDo.value);
     }
 })
